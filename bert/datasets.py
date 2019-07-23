Half precision, twitt dataset fixes. Random score for every metric added.

bert/datasets.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import os
 import csv
 from utils import remove_urls
+from itertools import groupby
 
 class UbuntuCorpus(Dataset):
 	def __init__(self, tokenizer, dir='./dialogs', max_seq_len=512, _cnt=30):
@@ -69,7 +70,7 @@ class UbuntuCorpus(Dataset):
 
 
 class TwittCorpus(Dataset):
-	def __init__(self, tokenizer, path='corp (1).txt'):
+	def __init__(self, tokenizer, path='corp.txt', max_seq_len=512, max_dataset_size=100):
 		'''
 		Gets Path to TXT file in format
 		[CLS] Qestion [SEP] \n
@@ -77,14 +78,19 @@ class TwittCorpus(Dataset):
 		\n
 		...
 		'''
-		super(twitt_dataset, self).__init__()
+		super(TwittCorpus, self).__init__()
 		with open(path, 'r') as f:
 			reps = f.readlines()
 		dgs = [list(group) for k, group in groupby(reps, lambda x: x == '\n') if not k]
-		for i in range(len(dgs)):
-			dgs[i][0] = dgs[i][0].remove('[SEP]', '').rstrip()
-			dgs[i][1] = dgs[i][1].remove('[SEP]', '').rstrip()
-		self.qa_s = dgs
+		good = []
+		for el in dgs:
+			el[0] = el[0].replace('[SEP]', '').rstrip()
+			el[1] = el[1].replace('[SEP]', '').rstrip()
+			if max(len(tokenizer.tokenize(el[0])), len(tokenizer.tokenize(el[1]))) <= max_seq_len:
+				good.append(el)
+				if len(good) == max_dataset_size:
+					break
+		self.qa_s = good
 
 	def __len__(self):
 		return len(self.qa_s)

bert/metrics.py
@@ -2,6 +2,7 @@ import numpy as np
 from losses import cosine_similarity_table
 from utils import prepare_batch, embed_batch
 import torch
+from random import *
 
 def calc_accuracy(X, Y):
 	csim = cosine_similarity_table(X, Y)
@@ -11,6 +12,9 @@ def calc_accuracy(X, Y):
 	for i in range(predictions.shape[0]):
 		correct += predictions[i] == i
 	return correct / X.shape[0]
+
+def calc_random_accuracy(batch_size):
+	return 1 / N_batch
 
 def calc_mrr(X, Y):
 	csim = cosine_similarity_table(X, Y)
@@ -27,7 +31,13 @@ def calc_mrr(X, Y):
 	assert(mrr <= 1.01)
 	return mrr
 
-def get_mean_score_on_data(metric, data, model, tokenizer):
+def calc_random_mrr(batch_size):
+	mrrs = 0
+	for i in range(1, 1 + batch_size):
+	    mrrs += 1 / i
+	return mrrs / batch_size
+
+def get_mean_score_on_data(metric, data, model, tokenizer, float_mode):
 	qembedder, aembedder = model
 	device = next(qembedder.parameters()).device
 
@@ -37,7 +47,7 @@ def get_mean_score_on_data(metric, data, model, tokenizer):
 	testbatch_cnt = 0
 	with torch.no_grad():
 		for batch in data:
-			embeddings = embed_batch(prepare_batch(batch, device, tokenizer), qembedder, aembedder)
+			embeddings = embed_batch(prepare_batch(batch, device, tokenizer), qembedder, aembedder, float_mode)
 			score += metric(*embeddings)
 			testbatch_cnt += 1
 	score /= testbatch_cnt

bert/train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from pytorch_transformers import BertTokenizer, BertForQuestionAnswering
 from pytorch_transformers import BertModel, BertConfig
@@ -15,7 +16,7 @@ import metrics
 import losses
 from utils import embed_batch, prepare_batch
 from utils import load_model, save_model
-from datasets import UbuntuCorpus
+from datasets import UbuntuCorpus, TwittCorpus
 
 from sacred import Experiment
 from sacred.observers import MongoObserver, TelegramObserver
@@ -24,7 +25,9 @@ import logging
 import os
 
 ex = Experiment()
-ex.observers.append(TelegramObserver.from_config("./aishutin-telegramobserver-config.json"))
+#ex.observers.append(TelegramObserver.from_config("./aishutin-telegramobserver-config.json"))
+
+writer = SummaryWriter()
 
 @ex.config
 def config():
@@ -34,11 +37,14 @@ def config():
 	epochs = 3
 	warmup = 0.1
 
-	metric_func = 'calc_mrr'
+	metric_name = 'mrr'
+	metric_func = f'calc_{metric_name}'
+	metric_baseline_func = f'calc_random_{metric_name}'
 	criterion_func = 'hinge_loss'
-	batch_size = 10 # 16, 32 are recommended in the paper
+	batch_size = 16 # 16, 32 are recommended in the paper
 
-	max_dataset_size = int(1e2)
+	float_mode = 'fp32'
+	max_dataset_size = int(1e4)
 
 	# BERT config
 	max_seq_len = 512
@@ -47,7 +53,7 @@ def config():
 
 @ex.capture
 def get_data(_log, data_path, tokenizer, test_split, max_seq_len, batch_size, max_dataset_size):
-	corpus = UbuntuCorpus(tokenizer, '../dialogs', max_seq_len, max_dataset_size)
+	corpus = TwittCorpus(tokenizer, '../corp.txt', max_seq_len, max_dataset_size)
 	_log.info(f'Corpus length: {len(corpus)}')
 	test_size = int(len(corpus) * test_split)
 	train_size = len(corpus) - test_size
@@ -66,12 +72,15 @@ def get_criterion(criterion_func):
 	return criterion
 
 @ex.capture
-def get_model(bert_type, cache_dir):
+def get_model(bert_type, cache_dir, float_mode):
 	tokenizer = BertTokenizer.from_pretrained(bert_type, cache_dir=cache_dir)
 	qembedder = BertModel.from_pretrained(bert_type, cache_dir=cache_dir)
 	aembedder = BertModel.from_pretrained(bert_type, cache_dir=cache_dir)
 	qembedder.config.output_hidden_states = True
 	aembedder.config.output_hidden_states = True
+	if float_mode == 'fp16':
+		qembedder.half()
+		aembedder.half()
 
 	model = (qembedder, aembedder)
 
@@ -79,7 +88,8 @@ def get_model(bert_type, cache_dir):
 
 
 @ex.automain
-def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metric_func, criterion_func):
+def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metric_func, \
+		metric_baseline_func, criterion_func, float_mode,):
 	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 	tokenizer, (qembedder, aembedder) = get_model()
@@ -89,6 +99,7 @@ def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metri
 	train, test = get_data(_log, '.', tokenizer)
 
 	metric = get_metric()
+	metric_baseline = get_metric(metric_baseline_func)
 	criterion = get_criterion()
 
 	num_train_optimization_steps = len(train) * epochs
@@ -105,12 +116,14 @@ def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metri
 
 	score_before_finetuning = metrics.get_mean_score_on_data(metric, test, \
 															(qembedder, aembedder), \
-															tokenizer)
+															tokenizer, float_mode)
 
 	_log.info("***** Running training *****")
 	_log.info("  Num steps = %d", num_train_optimization_steps)
 	_log.info(f"Score before fine-tuning: {score_before_finetuning:9.4f}")
+	_log.info(f'Random choice score: {metric_baseline(batch_size):9.4f}')
 
+	step = 0
 	for epoch in range(epochs):
 		total_loss = 0
 		qembedder.train()
@@ -120,10 +133,12 @@ def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metri
 			qoptim.zero_grad()
 			aoptim.zero_grad()
 
-			embeddings = embed_batch(prepare_batch(batch, device, tokenizer), qembedder, aembedder)
+			embeddings = embed_batch(prepare_batch(batch, device, tokenizer), qembedder, aembedder, float_mode)
 			loss = criterion(*embeddings)
 
 			total_loss += loss.item()
+			writer.add_scalar('data/loss_dynamic', loss.item(), step)
+			step += 1
 			loss.backward()
 
 			qscheduler.step()
@@ -134,7 +149,10 @@ def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metri
 		# ToDo do something with score
 		score = metrics.get_mean_score_on_data(metric, test, \
 											(qembedder, aembedder), \
-											tokenizer)
+											tokenizer, float_mode)
+
+		writer.add_scalar('data/score', score, epoch)
+		writer.add_scalar('data/total_loss', total_loss, epoch)
 		_log.info(f'score:{score:9.4f} | loss:{total_loss:9.4f} ')
 		checkpoint_name = checkpoint_dir + f"epoch:{epoch:2d} {metric_func}:{score:9.4f} {criterion_func}:{total_loss:9.4f}/"
 		save_model((qembedder, aembedder), tokenizer, checkpoint_name)
@@ -146,3 +164,4 @@ def train(_log, epochs, batch_size, learning_rate, warmup, checkpoint_dir, metri
 	# ToDo save model
 
 ex.run()
+writer.close()

bert/utils.py
@@ -12,15 +12,8 @@ def remove_urls (vTEXT):
 
 def prepare_batch(batch, device, tokenizer):
 	(quests, answs) = batch
-	#print(quests)
 	quests = [tokenizer.convert_tokens_to_ids(tokenizer.tokenize(el)) for el in quests]
 	answs = [tokenizer.convert_tokens_to_ids(tokenizer.tokenize(el)) for el in answs]
-	#print(quests)
-	#print(quests)
-	#print(quests[0].shape)
-
-	#quest_segments = [torch.zeros(len(quests[i]), 1, device=device) for i in range(len(quests))]
-	#answ_segments = [torch.zeros(len(answs[i]), 1, device=device) for i in range(len(answs))]
 
 	quest_segments = [torch.tensor([[0 for i in range(len(quests[j]))]], device=device) \
 															for j in range(len(quests))]
@@ -29,9 +22,6 @@ def prepare_batch(batch, device, tokenizer):
 
 	quests = [torch.tensor([el], device=device) for el in quests]
 	answs = [torch.tensor([el], device=device) for el in answs]
-
-	#print(quests)
-	#print(quest_segments)
 
 	return ((quests, quest_segments), (answs, answ_segments))
 
@@ -46,14 +36,17 @@ def get_embedding(embeddings):
 
 	return result
 
-def embed_batch(batch, qembedder, aembedder):
+def embed_batch(batch, qembedder, aembedder, float_mode):
 	((quests, quest_segments), (answs, answ_segments)) = batch
 
-	tmp_quest = [get_embedding(qembedder(quests[i], quest_segments[i], )[0]) for i in range(len(quests))]
+	tmp_quest = [get_embedding(qembedder(quests[i], quest_segments[i])[0]) for i in range(len(quests))]
 	tmp_answ = [get_embedding(aembedder(answs[i], answ_segments[i])[0]) for i in range(len(answs))]
 
 	qembeddings = torch.cat(tmp_quest)
 	aembeddings = torch.cat(tmp_answ)
+
+	if float_mode == 'fp16':
+		return (qembeddings.half(), aembeddings.half())
 
 	return (qembeddings, aembeddings)
 